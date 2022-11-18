a3fca46afaf31b01d82d6bb017346977ef0763cb
Salvador Beltran <salbeltran1995@gmail.com>
2022-11-17 23:51:56 -0500

    sect btns active and animations

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 
 function PageTransitions() {
@@ -10,9 +10,29 @@ function PageTransitions() {
         sectBtn[i].addEventListener('click', function() {
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn';
+            this.className += ' active-btn';
         })
     }
+
+    // Sections Active
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            // remove selected from other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            // hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
